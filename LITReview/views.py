8e6de4eca215d6a701cd94db6b6af34033491444
Arnaud <>
2024-03-29 16:27:54 +0100

    flux update

--- LITReview/views.py
+++ LITReview/views.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from Ticket.models import Ticket, Review
 from CustomUser.models import UserFollows
 from django.urls import reverse_lazy
+from itertools import chain
+from django.db.models import CharField, Value
 
 
 class Home(CustomLoginRequired, TemplateView):
@@ -10,25 +12,26 @@ class Home(CustomLoginRequired, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # General parameter of the page
         context["app_name"] = "Flux"
         context["page_name"] = "LitReview"
         context["back_url"] = reverse_lazy("home")
 
         user_follows = UserFollows.objects.filter(user=self.request.user)
-
-        # récuperer les tickets des utilisateurs suivis
         tickets = Ticket.objects.filter(
             user__in=user_follows.values_list("followed_user", flat=True)
         ) | Ticket.objects.filter(user=self.request.user)
-        context["tickets"] = tickets
+        tickets = tickets.annotate(content_type=Value("ticket", CharField()))
 
-        # récuperer les reviews des utilisateurs suivis ou de l'utilisateur ou des tickets créés par l'utilisateur
         reviews = (
             Review.objects.filter(ticket__in=tickets)
             | Review.objects.filter(user=self.request.user)
             | Review.objects.filter(ticket__user=self.request.user)
         )
-        context["reviews"] = reviews
+        reviews = reviews.annotate(content_type=Value("review", CharField()))
+
+        # Combiner les tickets et les critiques dans une seule liste
+        posts = sorted(chain(tickets, reviews), key=lambda post: post.time_created, reverse=True)
+        context["posts"] = posts
+        print(posts)
 
         return context
