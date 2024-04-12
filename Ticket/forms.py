39b180d49534df4dfe52d0e7ce949de5e1448f92
Arnaud Lienard <127483177+Laysi88@users.noreply.github.com>
2024-04-12 16:30:07 +0200

    correction visu modif ticket

--- Ticket/forms.py
+++ Ticket/forms.py
@@ -10,6 +10,13 @@ class TicketForm(forms.ModelForm):
             "description": forms.Textarea(attrs={"class": "text_area"}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields["image"].widget.initial_text = "Image actuelle"
+            self.fields["image"].widget.input_text = "Nouvelle image"
+            self.fields["image"].label = ""
+
 
 class ReviewToTIcketForm(forms.ModelForm):
     rating = forms.ChoiceField(widget=forms.RadioSelect, choices=[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)])
